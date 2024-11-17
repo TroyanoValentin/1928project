9471188ef0e5b6f4968978c38ab96ace0074cfbf
Losara3000 <losara308@gmail.com>
2024-11-17 18:06:00 +0700

    True Colors!!!!!

--- 1928project/Colors.h
+++ 1928project/Colors.h
@@ -1,1 +1,23 @@
-#pragma once
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+void RedStr(string str);
+void BlueStr(string str);
+void GreenStr(string str);
+void YellowStr(string str);
+void PurpleStr(string str);
+void CyanStr(string str);
+void WhiteStr(string str);
+void BlackStr(string str);
+void RedBg(string str);
+void BlueBg(string str);
+void GreenBg(string str);
+void YellowBg(string str);
+void PurpleBg(string str);
+void CyanBg(string str);
+void WhiteBg(string str);
+void BlackBg(string str);
+void Clear();
