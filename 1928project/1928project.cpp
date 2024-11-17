9d3a897f94a36e6815436d44761dcb883d69564f
TroyanoValentin <troyanovalentin@mail.ru>
2024-11-17 18:07:25 +0700

    лрп

--- 1928project/1928project.cpp
+++ 1928project/1928project.cpp
@@ -1,10 +1,10 @@
-﻿#include <iostream>
+﻿//Уважаемые друзья! Товарищи! Поздравляю всех нас с открытием первого общего командного проекта «1928»! Старт: 17.11.2024
+#include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
 
 using namespace std;
-
 int main(){
     
 }
