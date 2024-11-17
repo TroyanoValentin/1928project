ee1eeab444e7d49e071b0be199188c44c51b0839
Ernst-Muller <mashukoveand@gmail.com>
2024-11-17 18:21:18 +0700

    Первое заполнение Локаций и МАгазинов

---
+++ 1928project/Locations.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "Colors.h"
+#include "Text.h"
+#include "Pictures.h"
+#include "Stores.h"
+#include "Fight.h"
+
+using namespace std;
