64e0f358d0ed33e102d0292555cfa4ad5fb9cdff
fakie <fakie1488@outlook.com>
2024-11-30 23:30:35 +0700

    UPDATE IN DESIGN FROM INV

--- 1928project/Inventory.cpp
+++ 1928project/Inventory.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 class Inventory {
 protected:
-	vector <string> inventory = { " ”À¿ »", "œ¿—œŒ–“" };
+	string punch = "%========%\n|  ”À¿ » |\n%========%";
+	string pasport = "%=========%\n| œ¿—œŒ–“ |\n%=========%";
+	vector <string> inventory = { punch, pasport };
 public:
 	Inventory() {}
 	virtual void InvDo() const = 0;
@@ -18,9 +20,9 @@ class BaseInv : public Inventory {
 public:
 	void InvDo() const override {
 		cout << "[ ¬€ Œ“ –€À» »Õ¬≈Õ“¿–‹ ]";
-		cout << "\n¬¿ÿ »Õ¬≈Õ“¿–‹: ";
+		cout << "\n¬¿ÿ »Õ¬≈Õ“¿–‹: \n";
 		for (int i = 0; i < inventory.size(); i++) {
-			cout << "[" << inventory[i] << "] ";
+			cout << inventory[i] << "\n";
 		}
 	}
 };
