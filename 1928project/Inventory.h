fc7f5793d086b3d7388da7639fac3aed9dfb11c5
fakie <fakie1488@outlook.com>
2024-11-30 21:48:07 +0700

    робий инв для исп в мэин и др библио (именно чтобы его мождно было применять)

--- 1928project/Inventory.h
+++ 1928project/Inventory.h
@@ -5,3 +5,30 @@
 #include "Colors.h"
 
 using namespace std;
+
+class Inventory {
+protected:
+	vector <string> inventory = { " ”À¿ »", "œ¿—œŒ–“" };
+public:
+	Inventory() {}
+	virtual void InvDo() const = 0;
+};
+
+class BaseInv : public Inventory {
+public:
+	void InvDo() const override {
+		cout << "[ ¬€ Œ“ –€À» »Õ¬≈Õ“¿–‹ ]";
+		cout << "\n¬¿ÿ »Õ¬≈Õ“¿–‹: ";
+		for (int i = 0; i < inventory.size(); i++) {
+			cout << "[" << inventory[i] << "] ";
+		}
+	}
+};
+
+class CraftInv : public Inventory {
+
+};
+
+class CheckInv : public Inventory {
+
+};
