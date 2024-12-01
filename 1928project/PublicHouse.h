426b52137e5e349dffb9968c2acb7081a92c7b74
Losara3000 <losara308@gmail.com>
2024-12-01 19:14:26 +0700

    Base for PH

--- 1928project/PublicHouse.h
+++ 1928project/PublicHouse.h
@@ -3,5 +3,27 @@
 #include <string>
 #include <algorithm>
 #include "Colors.h"
+#include "Inventory.h"
 
 using namespace std;
+
+class PublicHouse {
+protected:
+	double dollars;
+	string sex_with;
+public:
+	PublicHouse() {}
+	virtual void Service() const = 0;
+};
+
+class s96 : public PublicHouse {
+
+};
+
+class sGoodJob : public PublicHouse {
+
+};
+
+class sHardCore : public PublicHouse {
+
+};
