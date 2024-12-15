675ed8abcf93c9e867e429a2921cdcb2a95342e7
Losara3000 <losara308@gmail.com>
2024-12-15 19:21:14 +0700

    PHub

--- 1928project/PublicHouse.cpp
+++ 1928project/PublicHouse.cpp
@@ -10,20 +10,41 @@ using namespace std;
 class PublicHouse {
 protected:
 	double dollars;
-	string sex_with;
 public:
 	PublicHouse() {}
 	virtual void Service() const = 0;
 };
 
 class s96 : public PublicHouse {
-
+	void Service() const override {
+		string sex_with = "мехгбеярмюъ фемыхмю [???]"; // мехгбеярмюъ фемыхмю [???] / мехгбеярмши лсфвхмю [???]
+	}
 };
 
 class sGoodJob : public PublicHouse {
-
+	void Service() const override {
+		string sex_with = "мехгбеярмюъ фемыхмю [???]"; // мехгбеярмюъ фемыхмю [???] / мехгбеярмши лсфвхмю [???]
+	}
 };
 
 class sHardCore : public PublicHouse {
+	void Service() const override {
+		string sex_with = "мехгбеярмши лсфвхмю[? ? ? ]"; // мехгбеярмюъ фемыхмю [???] / мехгбеярмши лсфвхмю [???]
+	}
+};
+
+void NoNameWoman96() {
+	cout << "\n[ бн бпелъ опнжеяяю бш пеьхкх нялнрперэ дебсьйс мюд бюлх, ]\n [ мн нянан мхвецн мнбнцн йпнле ыюбекъ бш ме сбхдекх ]\n";
+}
+
+void NoNameWomanGJ() {
+	cout << "\n[  ]";
+}
+
+void NoNameMan() {
+
+}
+
+class PHdo : public PublicHouse {
 
 };
