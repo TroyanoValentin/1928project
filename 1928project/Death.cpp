e3f1f375097226290b39590efaa676222b26774b
Maksim <marchenko061109@gmail.com>
2025-01-18 17:50:05 +0700

    Update Death.cpp
    Добавил функции выводящие смерти

---
+++ 1928project/Death.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+const void Les(string Les) {
+	Les = "Вы умерли заблужившись в лесу\n";
+  }
+
+const void Ytonyl(string Ytonyl) {
+	Ytonyl = "Вы умерли утонув в воде\n";
+}
+
+const void  Boy(string Boy) {
+	Boy = "Вы умерли утонув в воде\n";
+}
+
+const void Ifarkt(string Ifarkt) {
+	Ifarkt = "Вы умерли от инфаркта\n";
+}
+
+const void Vysota(string Vysota) {
+	Vysota = "Вы умерли упав с высоты\n";
+}
+using namespace std;
+int main()
+{
+  setlocale(LC_ALL, "Russian");
+}
